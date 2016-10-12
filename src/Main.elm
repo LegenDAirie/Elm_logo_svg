b5ed1c0f4433bfa61cbbb9d1347309e62ed020c9
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-10-12 12:26:08 -0700

    Remake of Elm logo

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,5 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import String
@@ -32,11 +31,34 @@ main =
         littleOrange =
             triangle 1
                 |> rotate -(90 + 45)
+
+        greenParallelagram =
+            parallelagram
+                |> rotate -45
+                |> snap 1 (to bigGrey 3)
+
+        greenSquare =
+            square
+                |> rotate -45
+
+        littleOrange2 =
+            triangle 1
+                |> rotate -45
+                |> snap 3 (to bigBlue 2)
+
+        midBlue =
+            triangle (sqrt 2)
+                |> rotate 90
+                |> snap 3 (to greenParallelagram 4)
     in
         svg [ viewBox "-2 -2 6 6" ]
             [ bigGrey |> draw colors.grey
             , bigBlue |> draw colors.blue
             , littleOrange |> draw colors.orange
+            , greenParallelagram |> draw colors.green
+            , greenSquare |> draw colors.green
+            , littleOrange2 |> draw colors.orange
+            , midBlue |> draw colors.blue
             ]
 
 
@@ -45,6 +67,24 @@ triangle size =
     [ ( 0, 0 )
     , ( size, 0 )
     , ( 0, size )
+    ]
+
+
+square : List Point
+square =
+    [ ( 0, 0 )
+    , ( 0, 1 )
+    , ( 1, 1 )
+    , ( 1, 0 )
+    ]
+
+
+parallelagram : List Point
+parallelagram =
+    [ ( 0, 0 )
+    , ( 0, 1 )
+    , ( 1, 2 )
+    , ( 1, 1 )
     ]
 
 
@@ -62,6 +102,40 @@ rotate angle pnts =
         List.map rotate' pnts
 
 
+to : List Point -> Int -> Point
+to pnts cornerNumber =
+    pnts
+        |> List.drop (cornerNumber - 1)
+        |> List.head
+        |> Maybe.withDefault ( 0, 0 )
+
+
+snap : Int -> Point -> List Point -> List Point
+snap cornerNumber target pnts =
+    case
+        pnts
+            |> List.drop (cornerNumber - 1)
+            |> List.head
+    of
+        Just pivot ->
+            pnts
+                |> sub pivot
+                |> add target
+
+        Nothing ->
+            pnts
+
+
+add : Point -> List Point -> List Point
+add ( dx, dy ) pnts =
+    List.map (\( x, y ) -> ( x + dx, y + dy )) pnts
+
+
+sub : Point -> List Point -> List Point
+sub ( x, y ) =
+    add ( -x, -y )
+
+
 draw : String -> List Point -> Svg msg
 draw color pnts =
     polygon
@@ -72,5 +146,7 @@ draw color pnts =
             |> String.join (",")
             |> points
         , fill color
+        , strokeWidth "0.05"
+        , stroke "white"
         ]
         []
